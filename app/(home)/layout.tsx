ff0713ffb882cdeaff5de1a40f17cfefdffb1f32
Chris Tate <chris@ctate.dev>
2026-01-02 13:41:55 -0600

    fix scroll

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -61,10 +61,10 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
   const isSignedIn = !!user;
 
   return (
-    <div className="h-dvh flex flex-col overflow-hidden bg-[#fafafa] text-black">
+    <div className="min-h-dvh flex flex-col bg-[#fafafa] text-black">
       <Header />
 
-      <main className="flex-1 min-h-0 overflow-y-auto">
+      <main className="flex-1">
         {/* Hero */}
         <section className="py-16 px-4">
           <div className="max-w-md mx-auto">
